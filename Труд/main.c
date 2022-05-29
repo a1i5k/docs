adf7e68834ae96ad84afd4080dc187ef9b2f5093
2afeu33f <rk6_student>
2022-05-29 13:52:36 +0300

    add_edumma

--- Труд/main.c
+++ Труд/main.c
@@ -2,22 +2,26 @@
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+
 #define H 6.0
 #define W 8.0
 #define h 2.0
 #define w 4.0
 #define T_LIQUID 100.0
 #define T_PIPE 10
-#define TIME_RUNNING 25
+#define TIME_RUNNING 2500
 #define A 1
+#define DX 0.1
+#define DY 0.1
 #define PATH_PLT "t.plt"
+#define PATH_RES "r.txt"
 
 void get_index(int index, int ny, int* ix, int* iy) {
     *iy = index % ny;
     *ix = index / ny;
 }
 
-void write_to_file(double** T, FILE* fds1, int ny, int nx) {
+void write_to_file(double** T, FILE* fds1, int ny, int nx, int lx, int ly) {
     FILE *fd;
     if ((fd = fopen(PATH_PLT,"w")) == NULL) {
         printf("Cannot open file!\n");
@@ -26,9 +30,9 @@ void write_to_file(double** T, FILE* fds1, int ny, int nx) {
 
     fprintf(fd, "set terminal png size 2000, 1000\n");
     fprintf(fd, "set output 'screen.png'\n");
-    fprintf(fd, "set palette defined (0 \"black\", 1\"blue\", 2 \"red\", 3 \"yellow\")\n");
-    fprintf(fd, "set xrange[0:25]\n");
-    fprintf(fd, "set yrange[0:19]\n");
+    fprintf(fd, "set palette defined (0 \"black\", 1\"blue\", 2 \"green\", 3 \"yellow\", 4 \"orange\", 5 \"red\")\n");
+    fprintf(fd, "set xrange[0:%d]\n", lx);
+    fprintf(fd, "set yrange[0:%d]\n", ly);
     fprintf(fd, "set cbrange[0:100]\n");
     fprintf(fd, "plot '-' using 1:2:3 with image notitle\n");
 
@@ -124,6 +128,9 @@ int main(int argc, char* argv[]) {
 
     initial_pipe(T, T_next, N, li, bj, tj, ri, ny);
 
+    hx = DX;
+    hy = DY;
+
     int i, j;
     for (int g = 0; g < TIME_RUNNING; g++) {
         double ht = 0;
@@ -162,27 +169,23 @@ int main(int argc, char* argv[]) {
             // Внешние границы
             // Левая и правая
             if (i == 0) {
-//                T_next[0][j] = (T[1][j]) / (1 + hx);
                 continue;
             }
 
             if (i == nx - 1) {
-//                T_next[i][j] = (T[i - 1][j]) / (1 + hx);
                 continue;
             }
             //нижняя и верхняя
             if (j == 0) {
-//                T_next[i][0] = (T[i][1]) / (1 + hy);
                 continue;
             }
 
             if (j == ny - 1) {
-//                T_next[i][j] = (T[i][j - 1]) / (1 + hy);
                 continue;
             }
 
             T_next[i][j] = ((T[i + 1][j] - 2 * T[i][j] + T[i - 1][j]) / (hx * hx) +
-                    (T[i][j + 1] - 2 * T[i][j] + T[i][j - 1]) / (hy * hy)) * A;
+                            (T[i][j + 1] - 2 * T[i][j] + T[i][j - 1]) / (hy * hy)) * A;
 
             if (ht < fabs(T_next[i][j])) {
                 ht = fabs(T_next[i][j]);
@@ -224,7 +227,6 @@ int main(int argc, char* argv[]) {
                 continue;
             }
         }
-
         T[0][0] = (T[0][1]) / (1 + hx);
         T[0][ny - 1] = (T[0][ny - 2]) / (1 + hy);
     }
@@ -235,9 +237,23 @@ int main(int argc, char* argv[]) {
         }
         printf("\n");
     }
+
+    FILE *fd;
+    if ((fd = fopen(PATH_RES,"w")) == NULL) {
+        printf("Cannot open file!\n");
+        exit(0);
+    }
+
+    for (j = ny - 1; j >= 0; j--) {
+        for (i = 0; i < nx; i++) {
+            fprintf(fd, "%8.3f", T[i][j]);
+        }
+        fprintf(fd, "\n");
+    }
+
     printf("\nhx = %g\nhy = %g\n", hx, hy);
 
-    write_to_file(T,file, ny, nx);
+    write_to_file(T,file, ny, nx, atoi(argv[1]) - 1, atoi(argv[2]) - 1);
 
     fclose(file);
 }
